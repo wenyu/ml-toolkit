d8ee2823f002162bf2eb74efc527c8f39be74b72
Wenyu Zhang <wyz@apple.com>
2018-04-03 19:02:03 -0700

    More generators.

--- mltk/generators.py
+++ mltk/generators.py
@@ -1,5 +1,6 @@
 import multiprocessing as mp
 import sys
+import numpy as np
 
 
 def fault_tolerant_endless_generator(lambda_iterable):
@@ -53,3 +54,31 @@ def parallel_map_generator(func, iterable, jobs=-1, batch_size=-1):
             result = pmap(func, batch)
             for result_item in result:
                 yield result_item
+
+
+def evenly_distributed_labeling_generator(X_data, Y_data):
+    data = {}
+    for x, y in zip(X_data, Y_data):
+        if y in data:
+            data[y].append(x)
+        else:
+            data[y] = [x]
+
+    gen = map(lambda (y, x): (fault_tolerant_endless_generator(lambda:x), y), data.items())
+
+    while True:
+        for x_gen, y in gen:
+            yield x_gen.next(), y
+
+
+def batch_x_y_generator(generator, batch_size=32):
+    X, Y = [], []
+    for x, y in generator:
+        X.append(x)
+        Y.append(y)
+        if len(Y) >= batch_size:
+            yield np.array(X), np.array(Y)
+            X, Y = [], []
+    else:
+        if len(Y) > 0:
+            yield np.array(X), np.array(Y)
