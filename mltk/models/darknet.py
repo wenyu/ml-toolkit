38b6158360abbe6f5934304811a2f4544bbb15a6
Wenyu Zhang <wyz@apple.com>
2018-04-17 14:10:17 -0700

    Fix Darknet Structure

--- mltk/models/darknet.py
+++ mltk/models/darknet.py
@@ -1,7 +1,7 @@
 import keras as K
 
 
-def Darknet19(input_shape=(224, 224, 3), name="", pooling=None, initial_kernel=5, initial_stride=2):
+def Darknet19(input_shape=(224, 224, 3), name="", pooling=None):
     layer_name = lambda s: name + "_" + s
 
     def conv_bn(suffix, x, filters, kernel_size, strides=1, alpha=0.1):
@@ -13,7 +13,7 @@ def Darknet19(input_shape=(224, 224, 3), name="", pooling=None, initial_kernel=5
         return y
 
     x = K.layers.Input(shape=input_shape, name=layer_name("input"))
-    y = conv_bn("1", x, 32, initial_kernel, initial_stride)
+    y = conv_bn("1", x, 32, 5)
     y = K.layers.MaxPool2D(strides=2, name=layer_name("pool_1"))(y)
     y = conv_bn("2", y, 64, 3)
     y = K.layers.MaxPool2D(strides=2, name=layer_name("pool_2"))(y)
@@ -21,17 +21,21 @@ def Darknet19(input_shape=(224, 224, 3), name="", pooling=None, initial_kernel=5
     y = conv_bn("3.2", y, 64, 1)
     y = conv_bn("3.3", y, 128, 3)
     y = K.layers.MaxPool2D(strides=2, name=layer_name("pool_3"))(y)
-    y = conv_bn("4.1", y, 512, 3)
-    y = conv_bn("4.2", y, 256, 1)
-    y = conv_bn("4.3", y, 512, 3)
-    y = conv_bn("4.4", y, 256, 1)
-    y = conv_bn("4.5", y, 512, 3)
+    y = conv_bn("4.1", y, 256, 3)
+    y = conv_bn("4.2", y, 128, 1)
+    y = conv_bn("4.3", y, 256, 3)
     y = K.layers.MaxPool2D(strides=2, name=layer_name("pool_4"))(y)
-    y = conv_bn("5.1", y, 1024, 3)
-    y = conv_bn("5.2", y, 512, 1)
-    y = conv_bn("5.3", y, 1024, 3)
-    y = conv_bn("5.4", y, 512, 1)
-    y = conv_bn("5.5", y, 1024, 3)
+    y = conv_bn("5.1", y, 512, 3)
+    y = conv_bn("5.2", y, 256, 1)
+    y = conv_bn("5.3", y, 512, 3)
+    y = conv_bn("5.4", y, 256, 1)
+    y = conv_bn("5.5", y, 512, 3)
+    y = K.layers.MaxPool2D(strides=2, name=layer_name("pool_5"))(y)
+    y = conv_bn("6.1", y, 1024, 3)
+    y = conv_bn("6.2", y, 512, 1)
+    y = conv_bn("6.3", y, 1024, 3)
+    y = conv_bn("6.4", y, 512, 1)
+    y = conv_bn("6.5", y, 1024, 3)
     y = K.layers.Conv2D(1000, 1, name=layer_name("conv_final"))(y)
 
     if pooling in ["avg", "average"]:
