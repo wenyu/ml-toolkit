9d4b3fefe2a14c425fcbc641a49291dd97e3ba2f
Wenyu Zhang <wyz@apple.com>
2018-04-17 13:52:36 -0700

    Change augment sequence

--- mltk/image_utils.py
+++ mltk/image_utils.py
@@ -22,15 +22,17 @@ __augseq_shape = [
         pad_mode=ia.ALL,
         pad_cval=(0, 255)
     )),
-    __often(iaa.Affine(
+    __sometimes(iaa.Affine(
         scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
         translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-        rotate=(-45, 45), # rotate by -45 to +45 degrees
-        shear=(-16, 16), # shear by -16 to +16 degrees
         order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
         cval=(0, 255), # if mode is constant, use a cval between 0 and 255
         mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
     )),
+    __occasionally(iaa.OneOf([
+        iaa.Affine(rotate=(-30, 30), mode=ia.ALL),
+        iaa.Affine(shear=(-15, 15), mode=ia.ALL),
+    ])),
     __seldom(iaa.OneOf([  # Occasionally rotate image 90 degress clockwise or counter-clockwise
         iaa.Affine(rotate=90),
         iaa.Affine(rotate=-90),
@@ -40,15 +42,15 @@ __augseq_shape = [
 ]
 __augseq_color = [
     __often(iaa.OneOf([
-        iaa.Add((-40, 40), per_channel=0.5),  # change brightness of images (by -40 to 40 of original value)
-        iaa.AddToHueAndSaturation((-40, 40)), # change hue and saturation
+        iaa.Add((-30, 30), per_channel=0.3),  # change brightness of images (by -30 to 30 of original value)
+        iaa.AddToHueAndSaturation((-40, 40)),  # change hue and saturation
     ])),
-    __occasionally(iaa.Multiply((0.5, 1.6), per_channel=0.5)),  # change the brightness with multiplication
+    __occasionally(iaa.Multiply((0.5, 1.6), per_channel=0.3)),  # change the brightness with multiplication
     iaa.Invert(0.005, per_channel=True),  # Invert Images
     __seldom(iaa.Grayscale(1.0)),
     __occasionally(iaa.ContrastNormalization((0.5, 1.5), per_channel=0.3)),
-    __seldom(iaa.Sharpen(alpha=(0.5, 1), lightness=(0.75, 1.5))),
-    __seldom(iaa.Emboss(alpha=1, strength=(0, 0.5)))
+    __rarely(iaa.Sharpen(alpha=(0.5, 1), lightness=(0.75, 1.5))),
+    __rarely(iaa.Emboss(alpha=1, strength=(0, 0.5)))
 ]
 __augseq_noise = [
     iaa.OneOf([
